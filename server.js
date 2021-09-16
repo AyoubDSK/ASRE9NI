07033ea369330c632d3a170d8d572740c0283145
ayoub cherguelaine <ayoubchergue09@gmail.com>
2021-09-16 16:16:53 +0200

    web
    web phase une

---
+++ server.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let app = express();
+const cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser');
+
+
+
+//middelware
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(cookieParser());
+
+app.get('/', function(req, res){
+    
+    res.render('./public/Home');
+})
+
+
+app.get('/Product', function(req,res){
+res.render('./public/ProductVitrine');
+
+})
+
+
+app.listen(8080);
+
