4eafc25de90b2ee23f16e99c6a0df1b53392bbf8
ayoub cherguelaine <ayoubchergue09@gmail.com>
2021-10-06 22:59:57 +0200

    pagination
    Fix pagination
    add Category , Brand

--- server.js
+++ server.js
@@ -63,7 +63,11 @@ app.get('/', function(req, res){
 
 function DownloadData(results, res,des){
 
-    let page = ProductNumber /10;
+    let page = ProductNumber /10 ;
+
+    // if( ProductNumber % 10 >0){
+    //     page+=1;
+    // }
 
 
 
@@ -88,9 +92,9 @@ function GetDataProduct(res,d,f,des){
  
    connection.connect();
    
-   console.log('select idProduct,Nom_Product,Price,Image,Describe_Product,P.idCategory,CatName,P.idBrand, BrandName from Product P join Brand B join Category C on P.idCategory= C.idCategory or P.idBrand=B.idBrand  limit '+d+','+f+'');
+   console.log('select idProduct,Nom_Product,Price,Image,Describe_Product,P.idCategory,CatName,P.idBrand, BrandName from Product P join Brand B join Category C on P.idCategory= C.idCategory and P.idBrand=B.idBrand  limit '+d+','+f+'')
    
-   connection.query('select idProduct,Nom_Product,Price,Image,Describe_Product,P.idCategory,CatName,P.idBrand, BrandName from Product P join Brand B join Category C on P.idCategory= C.idCategory or P.idBrand=B.idBrand  limit '+d+','+f+'', function (error, results, fields) {
+   connection.query('select idProduct,Nom_Product,Price,Image,Describe_Product,P.idCategory,CatName,P.idBrand, BrandName from Product P join Brand B join Category C on P.idCategory= C.idCategory and P.idBrand=B.idBrand  limit '+d+','+f+'', function (error, results, fields) {
        if (error) throw error;
    
    
@@ -138,6 +142,110 @@ app.get('/Product/Page=:num',function(req,res){
 app.get('/ProductTest',(req,res)=>{
     res.render('./public/Product/Test');
 });
+
+
+
+function resCategoryProduct(res,results,page){
+
+    let d= page*10;
+    let f= d+9;
+    let nbr;
+
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '12345',
+      database : 'Asre9niStore'
+    });
+     
+    connection.connect();
+    
+    
+
+
+    
+    connection.query(" select cout(*) as number from Product P join Brand B join Category C on P.idCategory= C.idCategory or P.idBrand=B.idBrand and CatName='"+Category+"'  ", function (error, results, fields) {
+        if (error) throw error;
+      
+    let nbr= results[0].number;
+
+        let nbrPage = nbr/10;
+
+        res.render('./public/ProductVitrine',{Products:results,Page:nbr,Des:page})
+    
+
+    
+    });
+
+    
+    connection.end();
+
+}
+
+function GetCategory(res,Category,page){
+
+
+    let d= page*10;
+    let f= d+9;
+    let nbr;
+
+    var mysql      = require('mysql');
+    var connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '12345',
+      database : 'Asre9niStore'
+    });
+     
+    connection.connect();
+    
+    
+
+
+    connection.query(" select idProduct,Nom_Product,Price,Image,Describe_Product,P.idCategory,CatName,P.idBrand, BrandName from Product P join Brand B join Category C on P.idCategory= C.idCategory or P.idBrand=B.idBrand and CatName='"+Category+"'  limit "+d+","+f+" ", function (error, results, fields) {
+        if (error) throw error;
+      
+        resCategoryProduct(res,results,page);
+    
+
+    
+    });
+    
+    
+    
+    
+    connection.end();
+    
+}
+
+
+app.get("/Product/Page=:num/Category=:Category",(req,res)=>{
+
+    let num=req.params.num;
+    let category=req.params.Category;
+
+
+    
+     
+});
+
+
+function GetCategoryBrand(res,Category,Brand,page){
+
+}
+
+app.get("/Product/Page=:num/Category=:Category/Brand=:Brand",(req,res)=>{
+
+    let num=req.params.num;
+    let category=req.params.Category;
+    let brand=req.params.Brand;
+    
+
+
+     
+});
+
 
 app.listen(8080);
 
