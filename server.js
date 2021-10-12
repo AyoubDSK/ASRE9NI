47ed9d4c67f9b004ac01c079db058e3814a6eb51
ayoub cherguelaine <ayoubchergue09@gmail.com>
2021-10-12 12:11:41 +0200

    Add Product
    Add Product EJS

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 const cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser');
-
+var multer = require('multer');
+var upload = multer();
 
 
 //middelware
@@ -15,6 +16,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cookieParser());
+
+
+
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+
+		// Uploads is the Upload_folder_name
+		cb(null, "public/images")
+	},
+	filename: function (req, file, cb) {
+	cb(null, file.fieldname + "-" + Date.now()+".jpg")
+	}
+})
+	
+// Define the maximum size for uploading
+// picture i.e. 1 MB. it is optional
+const maxSize = 1 * 1000 * 1000;
+	
+var upload = multer({
+	storage: storage,
+	limits: { fileSize: maxSize },
+	fileFilter: function (req, file, cb){
+	
+		// Set the filetypes, it is optional
+		var filetypes = /jpeg|jpg|png/;
+		var mimetype = filetypes.test(file.mimetype);
+
+		var extname = filetypes.test(path.extname(
+					file.originalname).toLowerCase());
+		
+		if (mimetype && extname) {
+			return cb(null, true);
+		}
+	
+		cb("Error: File upload only supports the "
+				+ "following filetypes - " + filetypes);
+	}
+
+// mypic is the name of file attribute
+}).single("ImagePath");	
+
+//router 
+
 
 
 
@@ -327,6 +371,18 @@ app.get("/Admin",(req,res)=>{
 
     res.render("./Admin/LogAdmin");
 
+})
+
+app.get("/AdminTest",(req,res)=>{
+    res.render("./Admin/AddProduct");
+});
+
+
+app.post("/AdminTest",(req,res)=>{
+
+    console.log(req.body);
+
+    res.end();
 })
 
 
